5f65b59d0f40408cda902b4707dd15bf33ed16be
siyaraam <siyaraam80@gmail.com>
2024-08-10 00:27:23 +0530

    my code

--- nykaa/nykaa/final project/my-project/src/pages/login/index.jsx
+++
@@ -1,7 +0,0 @@
-import "./Login.scss";
-
-function Login() {
-  return <div>Login</div>;
-}
-
-export default Login;
--- nykaa/nykaa/final project/my-project/src/pages/register/index.jsx
+++
@@ -1,7 +0,0 @@
-import "./Register.scss";
-
-function Register() {
-  return <div>Register</div>;
-}
-
-export default Register;
